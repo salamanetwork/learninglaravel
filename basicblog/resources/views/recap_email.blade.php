1d09cf9c952be3ac3d7289d02e7713a5309349d3
Ahmed Salama <salamanetwork@gmail.com>
2023-09-23 20:37:55 +0400

    modified:   basicblog/app/Console/Kernel.php 	new file:   basicblog/app/Mail/RecapEmail.php 	new file:   basicblog/resources/views/recap_email.blade.php

---
+++ basicblog/resources/views/recap_email.blade.php
@@ -0,0 +1,20 @@
+<div style="font-family: sans-serif">
+    <p>
+        {{-- <strong>
+            <span style="color: rgb(255, 0, 0)">{{$name}}</span>
+        </strong> --}}
+        Here is a quick recap of the application:
+    </p>
+    <p>
+        Post Count:
+        <strong>
+        <span style="color: rgb(255, 0, 0)">{{$postCount}}</span>
+         </strong>
+    </p>
+    <p>
+        User Count:
+        <strong>
+        <span style="color: rgb(255, 0, 0)">{{$userCount}}</span>
+         </strong>
+    </p>
+</div>
